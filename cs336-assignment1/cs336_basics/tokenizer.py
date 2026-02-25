2e276250615f265c5cacb310980fb8cf590683fe
calvin daniel woo <calvin.d.woo@gmail.com>
2026-02-25 01:07:48 -0500

    pretokenizer

--- cs336-assignment1/cs336_basics/tokenizer.py
+++ cs336-assignment1/cs336_basics/tokenizer.py
@@ -1,6 +1,28 @@
 from typing import TypeAlias
+import regex as re
 
 WordFreqs: TypeAlias = dict[tuple[bytes, ...], int]
+
+
+def pretokenize(text: str, special_tokens: list[str]) -> WordFreqs:
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    word_freqs: WordFreqs = {}
+
+    # there should always be some special tokens, such as <|endoftext|>
+    assert len(special_tokens) > 0
+    special_split_pattern = "|".join(map(re.escape, special_tokens))
+    chunks: list[str] = re.split(special_split_pattern, text)
+    for chunk in chunks:
+        for match in re.finditer(PAT, chunk):
+            match_grp = match.group()
+            match_byteseq = match_grp.encode("utf-8")
+            match_tokens = tuple(bytes([b]) for b in match_byteseq)
+
+            if match_tokens not in word_freqs:
+                word_freqs[match_tokens] = 0
+            word_freqs[match_tokens] += 1
+
+    return word_freqs
 
 
 def apply_merge(word_freqs: WordFreqs, pair_to_merge: tuple[bytes, bytes]) -> WordFreqs:
@@ -13,6 +35,7 @@ def apply_merge(word_freqs: WordFreqs, pair_to_merge: tuple[bytes, bytes]) -> Wo
         skip_next = False
         for wtoken_A, wtoken_B in zip(word, word[1:]):
             if skip_next:
+                skip_next = False
                 continue
             if wtoken_A == token_A and wtoken_B == token_B:
                 new_word.append(new_token)
@@ -25,6 +48,6 @@ def apply_merge(word_freqs: WordFreqs, pair_to_merge: tuple[bytes, bytes]) -> Wo
             new_word.append(word[-1])
 
         new_word = tuple(new_word)
-        new_word_freqs[new_word] = freqs
+        new_word_freqs[new_word] = freq
 
     return new_word_freqs
