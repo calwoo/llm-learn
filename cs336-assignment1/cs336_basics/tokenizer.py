a1b97a63e1fe3aedb80b20ba390a47594c21adb3
calvin daniel woo <calvin.d.woo@gmail.com>
2026-02-25 22:27:33 -0500

    working on tokenizer

--- cs336-assignment1/cs336_basics/tokenizer.py
+++ cs336-assignment1/cs336_basics/tokenizer.py
@@ -1,12 +1,17 @@
+from typing import Iterable, Iterator
 from typing import TypeAlias
 import regex as re
+import os
 
 WordFreqs: TypeAlias = dict[tuple[bytes, ...], int]
 Vocab: TypeAlias = dict[int, bytes]
 
 
+# GPT-2 pretokenizer
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+
 def pretokenize(text: str, special_tokens: list[str]) -> WordFreqs:
-    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
     word_freqs: WordFreqs = {}
 
     # there should always be some special tokens, such as <|endoftext|>
@@ -54,14 +59,16 @@ def apply_merge(word_freqs: WordFreqs, pair_to_merge: tuple[bytes, bytes]) -> Wo
     return new_word_freqs
 
 
-def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tuple[Vocab, list[tuple[bytes, bytes]]]:
+def train_bpe(
+    input_path: str | os.PathLike, vocab_size: int, special_tokens: list[str]
+) -> tuple[Vocab, list[tuple[bytes, bytes]]]:
     # initial vocab with individual bytes + special tokens
     vocab: Vocab = {i: bytes([i]) for i in range(256)}
     for tok in special_tokens:
         vocab[len(vocab)] = tok.encode("utf-8")
 
     # read text and pretokenize
-    with open(input_path, "r", encoding="utf-8") as f:
+    with open(input_path, encoding="utf-8") as f:
         text = f.read()
     word_freqs: WordFreqs = pretokenize(text, special_tokens)
 
@@ -78,7 +85,7 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tu
             break
 
         # get the highest count and merge
-        pair_to_merge = max(pair_counts, key=pair_counts.get)
+        pair_to_merge = max(pair_counts, key=lambda p: (pair_counts[p], p))
         word_freqs = apply_merge(word_freqs, pair_to_merge)
 
         # update vocab
@@ -88,3 +95,82 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tu
         merges.append(pair_to_merge)
 
     return vocab, merges
+
+
+class Tokenizer:
+    def __init__(self, vocab: Vocab, merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = None
+        if special_tokens:
+            self.special_tokens = list(sorted(special_tokens, key=len, reverse=True))
+
+        self.inverse_vocab: dict[bytes, int] = {}
+        for idx, token in vocab.items():
+            self.inverse_vocab[token] = idx
+
+    @classmethod
+    def from_files(
+        cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None
+    ) -> "Tokenizer": ...
+
+    def encode(self, text: str) -> list[int]:
+        # there should always be some special tokens, such as <|endoftext|>
+        chunks = [text]
+
+        if self.special_tokens is not None:
+            special_split_pattern = "|".join(map(re.escape, self.special_tokens))
+            chunks: list[str] = re.split(rf"({special_split_pattern})", text)
+
+        token_seq = []
+        for chunk in chunks:
+            if self.special_tokens and chunk in self.special_tokens:
+                token_idx = self.inverse_vocab[chunk.encode("utf-8")]
+                token_seq.append(token_idx)
+                continue
+
+            # if not special token, pretokenize
+            for match in re.finditer(PAT, chunk):
+                match_grp = match.group()
+                match_byteseq = match_grp.encode("utf-8")
+                match_tokens = tuple(bytes([b]) for b in match_byteseq)
+
+                merged_tokens = self._apply_merges(list(match_tokens))
+                # get idx
+                merged_token_seq = [self.inverse_vocab[tok] for tok in merged_tokens]
+                token_seq.extend(merged_token_seq)
+
+        return token_seq
+
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]: ...
+
+    def decode(self, ids: list[int]) -> str:
+        decoded_bytes = [self.vocab[idx] for idx in ids]
+        return b"".join(decoded_bytes).decode("utf-8", errors="replace")
+
+    def _apply_merges(self, token_seq: list[bytes]) -> list[bytes]:
+        for merge_A, merge_B in self.merges:
+            if len(token_seq) == 1:
+                return token_seq
+
+            new_token_seq = []
+            skip_next = False
+            for token_A, token_B in zip(token_seq, token_seq[1:]):
+                if skip_next:
+                    skip_next = False
+                    continue
+
+                if token_A == merge_A and token_B == merge_B:
+                    new_token = merge_A + merge_B
+                    new_token_seq.append(new_token)
+                    skip_next = True
+                else:
+                    new_token_seq.append(token_A)
+
+            # add in last token
+            if not skip_next:
+                new_token_seq.append(token_seq[-1])
+
+            token_seq = new_token_seq
+
+        return token_seq
--- cs336-assignment1/tests/adapters.py
+++ cs336-assignment1/tests/adapters.py
@@ -9,6 +9,8 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
+from cs336_basics.tokenizer import train_bpe, Tokenizer
+
 
 def run_linear(
     d_in: int,
@@ -559,7 +561,8 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+
+    return Tokenizer(vocab=vocab, merges=merges, special_tokens=special_tokens)
 
 
 def run_train_bpe(
@@ -589,4 +592,6 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+
+    vocab, merges = train_bpe(input_path=input_path, vocab_size=vocab_size, special_tokens=special_tokens)
+    return vocab, merges
